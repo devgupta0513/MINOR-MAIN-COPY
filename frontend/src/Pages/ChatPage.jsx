437bed5d75151658f69e39ddeaa064b2bcfb0d9b
Dev Gupta <devgupta210513@acropolis.in>
2023-10-31 18:24:45 +0530

    api of creating and searching

--- frontend/src/Pages/ChatPage.jsx
+++ frontend/src/Pages/ChatPage.jsx
@@ -1,14 +1,14 @@
 import { Box } from "@chakra-ui/layout"
-import { ChatState } from "../Context/ChatProvider"
+ 
 import SideDrawer from "../components/miscellaneous/SideDrawer"
 
  
 const ChatPage = () => {
-  const {user }= ChatState()      
+  // const {user }= ChatState()      
   
   return ( 
     <div style={{width:'100%'}}> 
-    {user && <SideDrawer/> }
+    {/* {user && <SideDrawer/> } */}
     <Box>
           {/* {user && <MyChats/> } */}
           {/* {user && <ChatBox/> } */}
