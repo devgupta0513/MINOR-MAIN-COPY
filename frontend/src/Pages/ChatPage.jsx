82237577967a0a92145fe23c24db1b91ff2e9f8a
Dev Gupta <devgupta210513@acropolis.in>
2023-10-31 21:10:11 +0530

    merging

--- frontend/src/Pages/ChatPage.jsx
+++ frontend/src/Pages/ChatPage.jsx
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-import { Box } from "@chakra-ui/layout"
- 
-// import SideDrawer from "../components/miscellaneous/SideDrawer"
-=======
+
 import { useContext } from 'react';
 import { createContext} from "react";
 import {Box} from '@chakra-ui/react'
->>>>>>> dev
 
  import SideDrawer from '../components/miscellaneous/SideDrawer';
 import MyChats from '../components/miscellaneous/MyChats';
